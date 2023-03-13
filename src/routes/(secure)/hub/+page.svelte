6d8132271281489505889939fc535af1d5fe4dae
Reza Banankhah <banankha@usc.edu>
2023-03-13 11:32:00 -1000

    Switched page locations
    Moved welcome page into its own secured route as courses wouldn't be loaded if the user wasn't logged in

---
+++ src/routes/(secure)/hub/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { courses } from '$lib/stores';
+</script>
+
+<main class="w-[calc(100vw-4rem)] relative">
+	<h1>Welcome to Homeschool Hub!</h1>
+
+	{#if $courses.length === 0}
+		<p>Loading courses...</p>
+	{:else}
+		<ul>
+			{#each $courses as course}
+				<li><a href={`course/${course.id}`}>{course.name}</a></li>
+			{/each}
+		</ul>
+	{/if}
+</main>
