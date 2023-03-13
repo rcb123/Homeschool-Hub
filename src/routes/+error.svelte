b89e8f0665784397c57d0feb73cbc77b4ba4817c
Reza Banankhah <banankha@usc.edu>
2023-03-13 11:28:58 -1000

    Created an Error Page
    Displays an error message for the user
    Works for all types of errors
    Styled properly

---
+++ src/routes/+error.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<main class="flex flex-col h-screen w-[calc(100vw-4rem)]">
+	<div
+		class="m-auto px-8 pt-4 pb-8 max-w-[20vw] min-w-[30rem] shadow-accent-focus shadow-lg rounded-2xl bg-neutral overflow-auto text-neutral-content text-center"
+	>
+		<h1 class="text-lg py-2">Something went wrong! 🤕</h1>
+		<p class="py-2">
+			Sorry about that, please try again later or feel free to reach out to us if the problem
+			persists.
+		</p>
+		<div class="flex flex-row justify-evenly py-4">
+			<a href="/hub">
+				<button class="btn btn-accent text-neutral-content normal-case">Head Back</button>
+			</a>
+			<a href="/report">
+				<button class="btn btn-primary normal-case">Submit a Support Request</button>
+			</a>
+		</div>
+		<p>Error: &#91;{$page.status}&#93; An error has occurred: {$page.error.message}</p>
+	</div>
+</main>
