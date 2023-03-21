3508a8cb99054c37efe57a7659fa06440ce29839
Reza Banankhah <banankha@usc.edu>
2023-03-21 00:53:57 -0700

    Update landing page
    Changed to use a hero element

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,19 +8,25 @@
 </script>
 
 <main>
-	<h1 class="text-4xl font-bold mb-4">Welcome to Homeschool Hub!</h1>
-	<p class="text-lg text-gray-700 mb-8">
-		Homeschool Hub is an online platform that connects homeschooling parents and students through a
-		centralized communication system. Our platform offers a range of features including
-		communication channels, lesson planning tools, a resource library, and community forums.
-	</p>
+	<div class="hero min-h-screen bg-base-200">
+		<div class="hero-content text-center">
+			<div class="max-w-md">
+				<h1 class="text-5xl font-bold">Welcome to Homeschool Hub!</h1>
+				<p class="py-6">
+					Homeschool Hub is an online platform that connects homeschooling parents and students
+					through a centralized communication system. Our platform offers a range of features
+					including communication channels, lesson planning tools, a resource library, and community
+					forums.
+				</p>
+				<a href="/register" class="btn btn-primary">Register Now</a>
+			</div>
+		</div>
+	</div>
 	<h2 class="text-2xl font-bold mb-4">Our Aims</h2>
 	<ul class="list-disc pl-6 mb-8">
 		{#each aims as aim}
 			<li>{aim}</li>
 		{/each}
 	</ul>
-	<a href="/register" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-		>Register Now</a
-	>
+	
 </main>
