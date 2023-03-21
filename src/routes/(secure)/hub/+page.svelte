27dd1fdf534a25c2e15a644b358260432d2359eb
Reza Banankhah <banankha@usc.edu>
2023-03-21 00:51:57 -0700

    Create hub page with conditional 'routing'

---
+++ src/routes/(secure)/hub/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { userRole } from '$lib/stores';
+	import TeacherHub from '$components/hubs/TeacherHub.svelte';
+	import StudentHub from '$components/hubs/StudentHub.svelte';
+
+	let role: 'student' | 'teacher' | null;
+
+	onMount(() => {
+		role = $userRole;
+	});
+</script>
+
+{#if role === 'teacher'}
+	<TeacherHub />
+{:else}
+	<StudentHub />
+{/if}
