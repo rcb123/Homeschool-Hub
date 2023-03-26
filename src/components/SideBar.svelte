e926df3207c4d230e74d10da52b87210ce7c180a
Reza Banankhah <banankha@usc.edu>
2023-03-25 23:39:55 -0700

    Update Side Bar
    Add settings icon/link
    Update logic to show enrolled courses as icons in the sidebar

--- src/components/SideBar.svelte
+++ src/components/SideBar.svelte
@@ -2,15 +2,15 @@
 	import SideBarIcon from './SideBarIcon.svelte';
 	import SideBarLogoIcon from './SideBarLogoIcon.svelte';
 
-	let servers: { name: string; id: string; logo: string; }[] = [];
+	let courses: { name: string; id: string; logo: string; }[] = [];
 </script>
 
 <div class="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-neutral text-neutral-content shadow-lg z-10">
 	<SideBarLogoIcon tooltip="Homeschool Hub" ref="/" />
 	<hr class="border-solid border-2 my-1 border-accent-focus" />
 
-	{#each servers as server}
-		<SideBarIcon icon={server.logo} tooltip={server.name} ref="/server/{server.id}" />
+	{#each courses as course}
+		<SideBarIcon icon={course.logo} tooltip={course.name} ref="/course/{course.id}" />
 	{/each}
 
 	<SideBarIcon icon="➕" tooltip="Add a Course" ref="/add" />
@@ -18,6 +18,7 @@
 
 	<!-- Sidebar footer -->
 	<div class="flex flex-col absolute w-full bottom-0 pb-2">
+		<SideBarIcon icon="⚙️" tooltip="Settings" ref="/settings" />
 		<SideBarIcon icon="📝" tooltip="Report a Problem" ref="/report" />
 		<SideBarIcon icon="💻" tooltip="Source Code" ref="https://github.com/rcb123/homeschool-hub" />
 	</div>
