24523fba0972bacb83f4c413362260a6eb99a565
Reza Banankhah <banankha@usc.edu>
2023-03-29 16:40:55 -0700

    Updated course page logic
    Updated course page data fetching to work with new supabase client

--- src/routes/(secure)/course/[courseID]/+page.ts
+++ src/routes/(secure)/course/[courseID]/+page.ts
@@ -1,21 +1,18 @@
 import type { PageLoad } from './$types';
-import { error } from '@sveltejs/kit';
-import { supabase } from '$lib/supabaseClient';
+import { error, redirect } from '@sveltejs/kit';
 import db from '$lib/stores.js';
 
-export const load = (async ({ params }) => {
-	const {
-		data: { session }
-	} = await supabase.auth.getSession();
-	if (session) {
-		await db.courses.getAll(session);
-
-		const course = await db.courses.getOne(params.courseID);
-		if (course) {
-			return course;
-		}
-		throw error(404, 'Course not found');
-	} else {
-		throw error(500, 'User not signed in');
+export const load: PageLoad = async ({ params, parent }) => {
+	const { supabase, session } = await parent();
+	if (!session) {
+		redirect(303, '/');
 	}
-}) satisfies PageLoad;
+
+	await db.courses.getAll(supabase, session);
+
+	const course = await db.courses.getOne(params.courseID);
+	if (!course) {
+		throw error(404, 'Course not found');
+	}
+	return course;
+};
